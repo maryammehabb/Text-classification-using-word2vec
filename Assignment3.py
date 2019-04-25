9da91392550a19499c01d51ecd88b22ef34d9ebd
Maryam Ehab <maryammehab@gmail.com>
2019-04-25 20:35:55 +0200

    ..

--- Assignment3.py
+++ Assignment3.py
@@ -3,53 +3,51 @@ import nltk
 import os
 import sys
 import numpy as np
-import pandas as pd
-
-#glove
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+#glove
 glovebath = 'glove.6B.50d.txt'
 
-#def read_data(file_name):
-with open(glovebath,'r', encoding="utf8") as f:
-        word_vocab = set() # not using list to avoid duplicate entry
+
+with open(glovebath, 'r', encoding="utf8") as f:
+        word_vocab = set()
         word2vector = {}
         for line in f:
-            line_ = line.strip() #Remove white space
+            line_ = line.strip()
             words_Vec = line_.split()
             #print(words_Vec[1])
             word_vocab.add(words_Vec[0])
-            word2vector[words_Vec[0]] = np.array(words_Vec[1:],dtype=float)
-print("Total Words in DataSet:",len(word_vocab))
+            word2vector[words_Vec[0]] = np.array(words_Vec[1:], dtype=float)
+print("Total Words in DataSet:", len(word_vocab))
 
 TEXT_DATA_DIR = 'txt_sentoken'
-texts = [] # list of text samples
-labels_index = {} # dictionary mapping label name to numeric id
-labels = [] # list of label ids
+texts = []
+labels_index = {}
+labels = []
 for name in sorted(os.listdir(TEXT_DATA_DIR)):
-    print(name)
     path = os.path.join(TEXT_DATA_DIR, name)
     if os.path.isdir(path):
         label_id = len(labels_index)
         labels_index[name] = label_id
         for fname in sorted(os.listdir(path)):
                 fpath = os.path.join(path, fname)
-                args = {} if sys.version_info < (3,) else {'encoding': 'latin-1'}
                 f = open(fpath, "r").read()
                 texts.append(f)
                 labels.append((label_id))
 
-'''print('Found %s texts.' % len(texts))
-print('nLabels = ', len(labels))
-print('Classes are:\n ')
+print('Labels = ', len(labels))
+print('Classes are:')
 for key in labels_index:
     print(key)
-print(labels_index)'''
 
 wordsOfDoc = []
 withoutpunc = []
+map = []
+map2 = []
+dictionary = {}
+dicvalues= []
 
 
 def removepunc(text):
@@ -63,12 +61,8 @@ def removepunc(text):
 for i in range(len(texts)):
     x = removepunc(texts[i])
     withoutpunc.append(x)
-print("Words without punctuation: ")
-print(len(withoutpunc))
-map = []
-map2 = []
-dictionary = {}
-dicvalues= []
+
+
 for i in range(len(withoutpunc)):
     word = nltk.word_tokenize(withoutpunc[i])
     wordsOfDoc.append(word)
@@ -76,23 +70,15 @@ for i in range(len(withoutpunc)):
         if word[j] in word2vector:
             dictionary[withoutpunc[i]] = np.sum(word2vector[word[j]])
 
-print(len(dictionary.values()))
-print(dictionary.values())
+
 for i in dictionary.values():
     dicvalues.append((i))
 
-print(len(dicvalues))
-print(dicvalues)
-print(labels)
-print(len(labels))
-'''print(texts)
-print(len(texts))
-print(labels)
-print(len(labels))'''
 dicvalues = ([dicvalues])
 labels = ([labels])
 print(labels)
 print(dicvalues)
+
 X_train, X_test, y_train, y_test = train_test_split(dicvalues, labels, test_size=0.25, random_state=42)
 model = LogisticRegression()
 model.fit(X_train, y_train)
