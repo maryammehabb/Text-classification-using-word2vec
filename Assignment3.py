fa49881c15876e012cb8a0eef9afd96e199431b7
Maryam Ehab <maryammehab@gmail.com>
2019-04-30 05:47:26 +0200

    Summation and average

--- Assignment3.py
+++ Assignment3.py
@@ -1,27 +1,21 @@
 import nltk
 import os
 import numpy as np
-import array as arr
 from string import punctuation
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 #glove
-glovebath = 'glove.6B/glove.6B.50d.txt'
-
+glovebath = 'glove.6B.50d.txt'
 with open(glovebath, 'r', encoding="utf8") as f:
-        word_vocab = set()
+        words = set()
         word2vector = {}
         for line in f:
             line_ = line.strip()
             words_Vec = line_.split()
-            #print(words_Vec[1])
-            word_vocab.add(words_Vec[0])
-            word2vector[words_Vec[0]] = np.array(words_Vec[1:], dtype=float)
-print("Total Words in DataSet:", len(word_vocab))
+            words.add(words_Vec[0])
+            word2vector[words_Vec[0]] = list(map(float, words_Vec[1:]))
+print("Total Words in DataSet:", len(words))
 
 TEXT_DATA_DIR = 'txt_sentoken'
 texts = []
@@ -35,20 +29,12 @@ for name in sorted(os.listdir(TEXT_DATA_DIR)):
         for fname in sorted(os.listdir(path)):
                 fpath = os.path.join(path, fname)
                 f = open(fpath, "r").read()
-                texts.append(f)
+                texts.append([f])
                 labels.append((label_id))
-
-print('Labels = ', len(labels))
 print('Classes are:')
 for key in labels_index:
     print(key)
-
-wordsOfDoc = []
 withoutpunc = []
-map = []
-map2 = []
-dictionary = {}
-dicvalues= []
 
 
 def removepunc(text):
@@ -61,57 +47,63 @@ def removepunc(text):
 
 for i in range(len(texts)):
     x = removepunc(texts[i])
-    withoutpunc.append(x)
+    withoutpunc.append([x])
 
-def sumWords():
-    for i in range(len(withoutpunc)):
-        word = nltk.word_tokenize(withoutpunc[i])
-        wordsOfDoc.append(word)
-        for j in range(len(word)):
-            if word[j] in word2vector:
-                dictionary[withoutpunc[i]] = np.sum(word2vector[word[j]])
-    return dictionary
 
-def avgWords():
-    for i in range(len(withoutpunc)):
-        word = nltk.word_tokenize(withoutpunc[i])
-        wordsOfDoc.append(word)
-        for j in range(len(word)):
-            if word[j] in word2vector:
-                dictionary[withoutpunc[i]] = np.average(word2vector[word[j]])
-    return dictionary
+def emmbedding(type, method):
+    result = []
+    wordsOfDoc = []
+    for i in range(len(type)):
+        #print(type[i])
+        tmp = []
+        for j in type[i]:
+            #print(j)
+            word = nltk.word_tokenize(j)
+            wordsOfDoc.append(word)
+            for p in word:
+                if p in word2vector:
+                    tmp.append(word2vector[p])
+                else:
+                    tmp.append([0]*50)
+            r = [sum(i) if method == "SUM" else sum(i)/len(i) for i in zip(*tmp)]
+            result.append(r)
+    return result
 
-print("1. Sentence embedding based on the sum of word embeddings")
-print("2. Sentence embedding based on the average of word embeddings")
-choice = input("Enter 1 or 2 please: ")
-if (choice == 1): sumWords()
-elif(choice == 2): avgWords()
 
-for i in dictionary.values():
-    dicvalues.append((i))
+text2 = []
+choice = input("Enter SUM or AVG please: ")
+text = input("Enter TEXT: ")
+text2 = [text]
+print(text2)
+x = emmbedding(withoutpunc, choice)
+y = []
+for i in labels:
+    y.append([i])
 
-dicvalues = ([dicvalues])
-labels =([labels])
-print(labels)
-print(dicvalues)
-
-#vectorizer = TfidfVectorizer(stop_words='english')
-
-X_train, X_test, y_train, y_test = train_test_split(dicvalues, labels, test_size=0.25, random_state=42)
-model = LogisticRegression()
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
+model = LogisticRegression(class_weight='balanced', C=1e7)
 model.fit(X_train, y_train)
 yPrediction = model.predict(X_test)
 print(yPrediction)
-x =accuracy_score(y_test, yPrediction)
-print(x)
-print("Accuracy: ", x*100," %")
+acc =accuracy_score(y_test, yPrediction)
+print(acc)
+print(acc*100)
 
-def Predict(v, m, text):
-    arroftext=[]
-    arroftext.append(text)
-    X_train = v.transform(arroftext)
-    result = m.predict(X_train)
-    print(result)
 
-text = input("Enter the text: ")
-#Predict(vectorizer, model, text)
+def predictt(text, m):
+    arroftext=[text]
+    print(arroftext)
+    dicpredict = emmbedding(arroftext, choice)
+    k = []
+    #print (len(dicpredict))
+    #print (dicpredict)
+    for i in dicpredict:
+        k.append([i])
+    o = m.predict(dicpredict)
+    print(o)
+
+
+predictt(text2, model)
+
+#drink and then drive
+#but my biggest kudos go out to sagemiller
